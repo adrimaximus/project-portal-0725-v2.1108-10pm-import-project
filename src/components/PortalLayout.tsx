63b4b6a1f498657c6d77f0092627e82be1de9907
adrimaximus <adri@7inked.com>
2025-07-26 22:29:48 +0700

    [dyad] Mengurangi margin vertikal pada layout - wrote 1 file(s)

--- src/components/PortalLayout.tsx
+++ src/components/PortalLayout.tsx
@@ -39,7 +39,7 @@ const PortalLayout = ({ children, noPadding = false, summary }: PortalLayoutProp
         </header>
         <main className={cn(
           "flex flex-1 flex-col overflow-auto",
-          !noPadding && "gap-4 px-4 py-2 lg:gap-6 lg:px-6 lg:py-4"
+          !noPadding && "gap-2 px-4 py-2 lg:gap-4 lg:px-6 lg:py-4"
         )}>
           {children}
         </main>
