e6b05f63bc4296e79620f04da59e68ef7bd8b37c
adrimaximus <adri@7inked.com>
2025-07-27 15:16:20 +0700

    Reverted all changes back to version ffd2555a4a1c7bb3be4a266f10f199a13d3afda5

--- src/components/request/ProjectDetailsForm.tsx
+++ src/components/request/ProjectDetailsForm.tsx
@@ -16,7 +16,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { DatePicker } from "@/components/ui/datepicker";
-import { allUsers as users } from "@/data/users";
+import { usersData as users } from "@/data/users";
 import { UserSelect } from "@/components/UserSelect";
 
 interface ProjectDetailsFormProps {
