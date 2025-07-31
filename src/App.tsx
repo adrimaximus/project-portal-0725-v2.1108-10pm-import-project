ffd988694bb4c40b6548a5ae33b0de5c996d51d7
adrimaximus <adri@7inked.com>
2025-07-31 20:04:36 +0700

    [dyad] Memperbaiki rute yang hilang - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from './pages/Index';
+import RequestPage from './pages/Request';
+import ChatPage from './pages/ChatPage';
+import MoodTracker from './pages/MoodTracker';
 import GoalsPage from './pages/GoalsPage';
 import GoalDetailPage from './pages/GoalDetailPage';
+import ProjectDetail from './pages/ProjectDetailPage';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import Billing from './pages/Billing';
 import NotFound from './pages/NotFound';
 import { GoalsProvider } from './context/GoalsContext';
 
@@ -11,8 +18,15 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/request" element={<RequestPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/mood-tracker" element={<MoodTracker />} />
           <Route path="/goals" element={<GoalsPage />} />
           <Route path="/goals/:goalId" element={<GoalDetailPage />} />
+          <Route path="/projects/:projectId" element={<ProjectDetail />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/billing" element={<Billing />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
