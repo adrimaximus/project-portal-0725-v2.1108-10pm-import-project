e379409a62c41decc07f66d8528cba5175c2a042
adrimaximus <adri@7inked.com>
2025-08-03 19:28:50 +0700

    [dyad] Memperbaiki kesalahan React Router - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import GoalDetailPage from './pages/GoalDetailPage';
 import GoalsPage from './pages/GoalsPage';
 import { Toaster } from '@/components/ui/sonner';
@@ -6,13 +6,11 @@ import { Toaster } from '@/components/ui/sonner';
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/goals" replace />} />
-          <Route path="/goals" element={<GoalsPage />} />
-          <Route path="/goals/:goalId" element={<GoalDetailPage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/goals" replace />} />
+        <Route path="/goals" element={<GoalsPage />} />
+        <Route path="/goals/:goalId" element={<GoalDetailPage />} />
+      </Routes>
       <Toaster richColors closeButton />
     </>
   );
