52a72f585ea406405b7609a44d1e7333bd7e4f63
adrimaximus <adri@7inked.com>
2025-08-03 15:52:26 +0700

    [dyad] Memperbaiki kesalahan perutean ganda - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,54 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { FeaturesProvider } from "@/contexts/FeaturesContext";
-import FeatureSettingsPage from "@/pages/FeatureSettingsPage";
-import Index from "@/pages/Index";
-import SettingsPage from "@/pages/Settings";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import Billing from "./pages/Billing";
+import ChatPage from "./pages/ChatPage";
+import EmbedPage from "./pages/EmbedPage";
+import FeatureSettingsPage from "./pages/FeatureSettingsPage";
+import GoalDetailPage from "./pages/GoalDetailPage";
+import GoalsPage from "./pages/GoalsPage";
+import Index from "./pages/Index";
 import IntegrationsPage from "./pages/IntegrationsPage";
+import MoodTracker from "./pages/MoodTracker";
+import NotFound from "./pages/NotFound";
+import NotificationsPage from "./pages/Notifications";
+import Profile from "./pages/Profile";
+import ProjectDetail from "./pages/ProjectDetail";
+import Projects from "./pages/Projects";
+import RequestPage from "./pages/Request";
+import SearchPage from "./pages/SearchPage";
+import SettingsPage from "./pages/Settings";
+import UserManagementPage from "./pages/UserManagement";
 
 function App() {
   return (
-    <FeaturesProvider>
-      <TooltipProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route
-              path="/settings/features/:featureId"
-              element={<FeatureSettingsPage />}
-            />
-            <Route path="/settings/integrations" element={<IntegrationsPage />} />
-          </Routes>
-        </Router>
-        <Toaster />
-      </TooltipProvider>
-    </FeaturesProvider>
+    <TooltipProvider>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:projectId" element={<ProjectDetail />} />
+        <Route path="/request" element={<RequestPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/mood-tracker" element={<MoodTracker />} />
+        <Route path="/goals" element={<GoalsPage />} />
+        <Route path="/goals/:goalId" element={<GoalDetailPage />} />
+        <Route path="/billing" element={<Billing />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/settings/features/:featureId"
+          element={<FeatureSettingsPage />}
+        />
+        <Route path="/settings/integrations" element={<IntegrationsPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/custom" element={<EmbedPage />} />
+        <Route path="/users" element={<UserManagementPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Toaster />
+    </TooltipProvider>
   );
 }
 
