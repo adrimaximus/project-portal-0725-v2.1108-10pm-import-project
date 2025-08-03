152f47fb8604cd6d56783c82cbcd719fc58911ef
adrimaximus <adri@7inked.com>
2025-08-03 17:46:51 +0700

    [dyad] Memperbaiki kesalahan UserProvider yang hilang - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { GoalsProvider } from './context/GoalsContext';
 import { SettingsProvider } from './context/SettingsContext';
+import { UserProvider } from './contexts/UserContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <SettingsProvider>
-        <GoalsProvider>
-          <App />
-        </GoalsProvider>
-      </SettingsProvider>
+      <UserProvider>
+        <SettingsProvider>
+          <GoalsProvider>
+            <App />
+          </GoalsProvider>
+        </SettingsProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
