f078cd247533cf7ef6179100281c53283c5b297b
adrimaximus <adri@7inked.com>
2025-08-03 21:15:53 +0700

    [dyad] Fixing nested React Router error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import DashboardPage from "./pages/DashboardPage"
 import ProjectsPage from "./pages/ProjectsPage"
 import ProjectDetailPage from "./pages/ProjectDetailPage"
@@ -10,31 +10,26 @@ import GoalEditPage from "./pages/GoalEditPage"
 import SettingsPage from "./pages/SettingsPage"
 import TeamPage from "./pages/TeamPage"
 import SearchPage from "./pages/SearchPage"
-import { GoalsProvider } from "./context/GoalsContext"
 import IntegrationsDirectoryPage from "./pages/integrations/IntegrationsDirectoryPage"
 import OpenAiIntegrationPage from "./pages/integrations/OpenAiIntegrationPage"
 
 function App() {
   return (
     <TooltipProvider>
-      <GoalsProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/projects/:id" element={<ProjectDetailPage />} />
-            <Route path="/goals" element={<GoalsPage />} />
-            <Route path="/goals/:id" element={<GoalDetailPage />} />
-            <Route path="/goals/:id/edit" element={<GoalEditPage />} />
-            <Route path="/goals/new" element={<GoalEditPage />} />
-            <Route path="/team" element={<TeamPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route path="/settings/integrations" element={<IntegrationsDirectoryPage />} />
-            <Route path="/settings/integrations/openai" element={<OpenAiIntegrationPage />} />
-            <Route path="/search" element={<SearchPage />} />
-          </Routes>
-        </Router>
-      </GoalsProvider>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/:id" element={<ProjectDetailPage />} />
+        <Route path="/goals" element={<GoalsPage />} />
+        <Route path="/goals/:id" element={<GoalDetailPage />} />
+        <Route path="/goals/:id/edit" element={<GoalEditPage />} />
+        <Route path="/goals/new" element={<GoalEditPage />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/settings/integrations" element={<IntegrationsDirectoryPage />} />
+        <Route path="/settings/integrations/openai" element={<OpenAiIntegrationPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
       <Toaster />
     </TooltipProvider>
   )
