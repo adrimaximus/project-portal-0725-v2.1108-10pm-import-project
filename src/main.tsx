d19ad5008be8f5db35edd2b83c43b467f17c953d
adrimaximus <adri@7inked.com>
2025-08-03 11:57:30 +0700

    [dyad] Fix missing User Management card - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom';
-import { UserProvider } from './contexts/UserContext.tsx';
-import { GoalsProvider } from './context/GoalsContext.tsx';
-import { FeaturesProvider } from './contexts/FeaturesContext.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { FeaturesProvider } from './contexts/FeaturesContext';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <UserProvider>
-        <FeaturesProvider>
-          <GoalsProvider>
-            <App />
-          </GoalsProvider>
-        </FeaturesProvider>
-      </UserProvider>
-    </Router>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <FeaturesProvider>
+        <App />
+      </FeaturesProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
