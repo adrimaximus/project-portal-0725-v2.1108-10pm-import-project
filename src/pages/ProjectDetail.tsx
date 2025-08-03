432e2ffeb92a47dc4e7a7cc5453a182d49dc7bb7
adrimaximus <adri@7inked.com>
2025-08-04 01:52:34 +0700

    [dyad] Mengaktifkan scroll vertikal untuk seluruh halaman - wrote 1 file(s)

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -214,11 +214,10 @@ const ProjectDetail = () => {
           canEdit={true}
         />
       }
-      disableMainScroll
       noPadding
     >
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 p-4 md:p-6 h-full">
-        <div className="lg:col-span-2 space-y-8 overflow-y-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 p-4 md:p-6">
+        <div className="lg:col-span-2 space-y-8">
           <ProjectInfoCards
             project={project}
             isEditing={isEditing}
@@ -235,7 +234,7 @@ const ProjectDetail = () => {
             onAddCommentOrTicket={handleAddCommentOrTicket}
           />
         </div>
-        <div className="lg:col-span-1 overflow-y-auto">
+        <div className="lg:col-span-1 space-y-8">
           <ProjectSidebar
             project={project}
             onUpdateProject={handleUpdateProjectDetails}
