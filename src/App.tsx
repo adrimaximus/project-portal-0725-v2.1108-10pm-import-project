c19044b73ecb6e3118ca796edf2ba02ef52652e6
adrimaximus <adri@7inked.com>
2025-08-03 17:46:21 +0700

    [dyad] Memperbaiki kesalahan React Router ganda - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Toaster } from "@/components/ui/sonner"
-import { GoalsProvider } from './context/GoalsContext';
-import { SettingsProvider } from './context/SettingsContext';
 import Layout from './components/layout/Layout';
 import IndexPage from './pages/Index';
 import GoalsPage from './pages/GoalsPage';
@@ -10,21 +8,17 @@ import SettingsPage from './pages/Settings';
 
 function App() {
   return (
-    <SettingsProvider>
-      <GoalsProvider>
-        <Router>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<IndexPage />} />
-              <Route path="/goals" element={<GoalsPage />} />
-              <Route path="/goals/:id" element={<GoalDetailPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
-            </Routes>
-          </Layout>
-        </Router>
-        <Toaster />
-      </GoalsProvider>
-    </SettingsProvider>
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/goals" element={<GoalsPage />} />
+          <Route path="/goals/:id" element={<GoalDetailPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </Layout>
+      <Toaster />
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { FeaturesProvider } from './contexts/FeaturesContext';
-import { UserProvider } from './contexts/UserContext';
 import { GoalsProvider } from './context/GoalsContext';
+import { SettingsProvider } from './context/SettingsContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <FeaturesProvider>
-          <GoalsProvider>
-            <App />
-          </GoalsProvider>
-        </FeaturesProvider>
-      </UserProvider>
+      <SettingsProvider>
+        <GoalsProvider>
+          <App />
+        </GoalsProvider>
+      </SettingsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
