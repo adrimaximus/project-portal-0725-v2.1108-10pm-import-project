4f15e75cf1138fe0325e74948ea3d3fc5b622f54
adrimaximus <adri@7inked.com>
2025-08-03 15:54:20 +0700

    [dyad] Menambahkan integrasi Google Drive - wrote 1 file(s)

--- src/pages/IntegrationsPage.tsx
+++ src/pages/IntegrationsPage.tsx
@@ -58,6 +58,17 @@ const IntegrationsPage = () => {
           <CardContent className="space-y-4">
             <IntegrationItem name="GitHub" description="Sync your repositories and issues." icon={<GitBranch className="h-5 w-5" />} />
             <IntegrationItem name="Slack" description="Get notifications in your Slack channels." icon={<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12.83 12.83a2 2 0 0 0-2.83 0 2 2 0 0 0 0 2.83 2 2 0 0 0 2.83 0 2 2 0 0 0 0-2.83z"></path><path d="M18.36 18.36a2 2 0 0 0 0-2.83 2 2 0 0 0-2.83 0 2 2 0 0 0 0 2.83 2 2 0 0 0 2.83 0z"></path><path d="M11.17 12.83a2 2 0 0 0 0-2.83 2 2 0 0 0-2.83 0 2 2 0 0 0 0 2.83 2 2 0 0 0 2.83 0z"></path><path d="M5.64 5.64a2 2 0 0 0 0 2.83 2 2 0 0 0 2.83 0 2 2 0 0 0 0-2.83 2 2 0 0 0-2.83 0z"></path><path d="M12.83 11.17a2 2 0 0 0-2.83 0 2 2 0 0 0 0 2.83 2 2 0 0 0 2.83 0 2 2 0 0 0 0-2.83z"></path><path d="M18.36 5.64a2 2 0 0 0 0 2.83 2 2 0 0 0 2.83 0 2 2 0 0 0 0-2.83 2 2 0 0 0-2.83 0z"></path><path d="M11.17 11.17a2 2 0 0 0 0 2.83 2 2 0 0 0 2.83 0 2 2 0 0 0 0-2.83 2 2 0 0 0-2.83 0z"></path><path d="M5.64 18.36a2 2 0 0 0 2.83 0 2 2 0 0 0 0-2.83 2 2 0 0 0-2.83 0 2 2 0 0 0 0 2.83z"></path></svg>} />
+            <IntegrationItem 
+                name="Google Drive" 
+                description="Access your files from Google Drive." 
+                icon={
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M8.25 14.25 3 22.5h7.5l5.25-8.25H8.25Z"/>
+                        <path d="m9.75 3-6.75 11.25h13.5L21 3H9.75Z"/>
+                        <path d="M21.75 14.25 16.5 22.5H24l-2.25-8.25h-7.5Z"/>
+                    </svg>
+                } 
+            />
           </CardContent>
         </Card>
       </div>
