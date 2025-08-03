35505dfa17ac1fd6bb971a4a223c19e119eb1881
adrimaximus <adri@7inked.com>
2025-08-03 19:29:08 +0700

    Reverted all changes back to version d624c7db0d289e5ddee42cbcce6e276aad18a226

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import GoalDetailPage from './pages/GoalDetailPage';
 import GoalsPage from './pages/GoalsPage';
 import { Toaster } from '@/components/ui/sonner';
@@ -6,11 +6,13 @@ import { Toaster } from '@/components/ui/sonner';
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navigate to="/goals" replace />} />
-        <Route path="/goals" element={<GoalsPage />} />
-        <Route path="/goals/:goalId" element={<GoalDetailPage />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/goals" replace />} />
+          <Route path="/goals" element={<GoalsPage />} />
+          <Route path="/goals/:goalId" element={<GoalDetailPage />} />
+        </Routes>
+      </Router>
       <Toaster richColors closeButton />
     </>
   );
