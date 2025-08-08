a5553bb51a606a103c9015c3cc5bf93bdc60d126
adrimaximus <adri@7inked.com>
2025-08-09 06:50:53 +0700

    [dyad] Fix useAuth error by adding AuthProvider - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -8,18 +8,21 @@ import { GoalsProvider } from './context/GoalsContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { GOOGLE_CLIENT_ID } from './config';
 import './index.css';
+import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <BrowserRouter>
-        <UserProvider>
-          <FeaturesProvider>
-            <GoalsProvider>
-              <App />
-            </GoalsProvider>
-          </FeaturesProvider>
-        </UserProvider>
+        <AuthProvider>
+          <UserProvider>
+            <FeaturesProvider>
+              <GoalsProvider>
+                <App />
+              </GoalsProvider>
+            </FeaturesProvider>
+          </UserProvider>
+        </AuthProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
