4c56eacb3f1a2ea9b148e72d0951964dcbbf467d
adrimaximus <adri@7inked.com>
2025-08-09 15:01:00 +0700

    [dyad] Memperbaiki integrasi Google Login dan Kalender - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { FeaturesProvider } from './contexts/FeaturesContext';
-import { GoalsProvider } from './context/GoalsContext';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import { FeaturesProvider } from './contexts/FeaturesContext';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GOOGLE_CLIENT_ID } from './config';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <FeaturesProvider>
-          <GoalsProvider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <AuthProvider>
+          <FeaturesProvider>
             <App />
-          </GoalsProvider>
-        </FeaturesProvider>
-      </AuthProvider>
-    </BrowserRouter>
+          </FeaturesProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
