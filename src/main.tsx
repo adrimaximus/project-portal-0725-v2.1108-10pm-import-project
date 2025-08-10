eb8293e938488efc29a12fa645175245ce434ff7
adrimaximus <adri@7inked.com>
2025-08-10 20:15:17 +0700

    [dyad] Memperbaiki alur login Google di localhost - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,15 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { FeaturesProvider } from './contexts/FeaturesContext';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GOOGLE_CLIENT_ID } from './config';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <AuthProvider>
-          <FeaturesProvider>
-            <App />
-          </FeaturesProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <FeaturesProvider>
+          <App />
+        </FeaturesProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
