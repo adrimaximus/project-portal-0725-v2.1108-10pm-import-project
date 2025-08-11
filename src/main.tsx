af3260afef68792fa618fc60514b639e543c86d7
adrimaximus <adri@7inked.com>
2025-08-11 09:35:14 +0700

    [dyad] Memperbaiki integrasi Google Kalender - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,19 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { FeaturesProvider } from './contexts/FeaturesContext';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GOOGLE_CLIENT_ID } from './config';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <FeaturesProvider>
-          <App />
-        </FeaturesProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <AuthProvider>
+          <FeaturesProvider>
+            <App />
+          </FeaturesProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
