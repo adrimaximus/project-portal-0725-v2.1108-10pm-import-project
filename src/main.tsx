fcbc126f043cbbcbd9ccf1699d6a756c76ad3518
adrimaximus <adri@7inked.com>
2025-08-12 15:18:12 +0700

    [dyad] Fixed Google OAuth provider error - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,24 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { FeaturesProvider } from './contexts/FeaturesContext';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+if (!GOOGLE_CLIENT_ID) {
+  console.error("Fatal Error: VITE_GOOGLE_CLIENT_ID is not defined in .env file. Google authentication will not work.");
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <FeaturesProvider>
-          <App />
-        </FeaturesProvider>
-      </AuthProvider>
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID || ""}>
+        <AuthProvider>
+          <FeaturesProvider>
+            <App />
+          </FeaturesProvider>
+        </AuthProvider>
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
