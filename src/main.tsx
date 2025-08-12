f5eae1e0b0bf9a82378ec25595d48326f398ba6d
adrimaximus <adri@7inked.com>
2025-08-12 14:19:43 +0700

    Reverted all changes back to version d7b22d3c3de0721c520592d905791c78795aaa00

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,15 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { FeaturesProvider } from './contexts/FeaturesContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <FeaturesProvider>
-            <App />
-          </FeaturesProvider>
-        </AuthProvider>
-      </QueryClientProvider>
+      <AuthProvider>
+        <FeaturesProvider>
+          <App />
+        </FeaturesProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
