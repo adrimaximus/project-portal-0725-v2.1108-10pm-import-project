c819e8f6245eb46640f15b8950dac2203f967bba
adrimaximus <adri@7inked.com>
2025-08-16 18:49:00 +0700

    [dyad] Set default user_id for new goals in the database - wrote 1 file(s), executed 1 SQL queries

--- src/components/goals/GoalFormDialog.tsx
+++ src/components/goals/GoalFormDialog.tsx
@@ -128,7 +128,6 @@ const GoalFormDialog = ({ open, onOpenChange, onSuccess, onGoalUpdate, goal }: G
     } else if (!isEditMode) {
       try {
         const goalInsertData = {
-          user_id: user.id,
           title,
           description,
           icon,
