d3b22ea9c66712f71dcfa51ba77a2797a422cd6e
adrimaximus <adri@7inked.com>
2025-08-19 21:06:17 +0700

    [dyad] Perbaikan warna mention dan konfirmasi fungsi tautan obrolan - wrote 2 file(s), deleted 1 file(s)

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,7 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        mention: "hsl(var(--mention))",
       },
       borderRadius: {
         lg: "var(--radius)",
