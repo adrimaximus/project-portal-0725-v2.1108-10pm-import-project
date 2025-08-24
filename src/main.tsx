d261530eecde4afe1e196cab00fdc537bfb215f3
adrimaximus <adri@7inked.com>
2025-08-25 04:55:41 +0700

    [dyad] Memperbaiki kesalahan kompilasi login Google - wrote 4 file(s), added @react-oauth/google, @react-oauth/google package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -8,21 +8,29 @@ import { FeaturesProvider } from './contexts/FeaturesContext'
 import { Toaster } from "@/components/ui/sonner"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from './contexts/ThemeProvider'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const queryClient = new QueryClient()
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+if (!googleClientId) {
+  throw new Error("VITE_GOOGLE_CLIENT_ID is not defined in the environment variables. Google authentication features will not work.");
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <FeaturesProvider>
-            <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-              <App />
-              <Toaster />
-            </ThemeProvider>
-          </FeaturesProvider>
-        </AuthProvider>
+        <GoogleOAuthProvider clientId={googleClientId}>
+          <AuthProvider>
+            <FeaturesProvider>
+              <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+                <App />
+                <Toaster />
+              </ThemeProvider>
+            </FeaturesProvider>
+          </AuthProvider>
+        </GoogleOAuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
