c223fcc774b5d5cf4f832839f4b058dbd48e70ce
adrimaximus <adri@7inked.com>
2025-09-22 19:37:23 +0700

    [dyad] Memperbaiki masalah penyegaran halaman dengan HashRouter - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
@@ -14,7 +14,7 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <FeaturesProvider>
@@ -34,6 +34,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           </FeaturesProvider>
         </AuthProvider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
