800cd4adfc7ca33b4c6123a1335b65a4d709b5eb
adrimaximus <adri@7inked.com>
2025-09-30 18:19:00 +0700

    [dyad] Fixed nested router error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import IntegrationsPage from "./pages/IntegrationsPage";
 import GoogleCalendarIntegrationPage from "./pages/GoogleCalendarIntegrationPage";
 import GoogleDriveIntegrationPage from "./pages/GoogleDriveIntegrationPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/settings/integrations" />} />
-        <Route path="/settings/integrations" element={<IntegrationsPage />} />
-        <Route path="/settings/integrations/google-calendar" element={<GoogleCalendarIntegrationPage />} />
-        <Route path="/settings/integrations/google-drive" element={<GoogleDriveIntegrationPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/settings/integrations" />} />
+      <Route path="/settings/integrations" element={<IntegrationsPage />} />
+      <Route
+        path="/settings/integrations/google-calendar"
+        element={<GoogleCalendarIntegrationPage />}
+      />
+      <Route
+        path="/settings/integrations/google-drive"
+        element={<GoogleDriveIntegrationPage />}
+      />
+    </Routes>
   );
 }
 
