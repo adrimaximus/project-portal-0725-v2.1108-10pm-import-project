8a0a7f93cf78ad8c9408b3e52e647585f05ebc07
adrimaximus <adri@7inked.com>
2025-10-02 18:12:29 +0700

    [dyad] Fix nested Router error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { GlobalCommand } from './components/GlobalCommand';
 import Index from './pages/Index';
 
 function App() {
   return (
-    <Router>
+    <>
       <GlobalCommand />
       <Routes>
         <Route path="/" element={<Index />} />
         {/* Redirect other paths to home for now, can be expanded */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
