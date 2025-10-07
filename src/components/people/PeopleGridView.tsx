e750fdfcf8bb9f868b100a4bdbb3a59d1859bed6
adrimaximus <adri@7inked.com>
2025-10-07 21:09:31 +0700

    [dyad] Memperbarui tata letak grid untuk seluler - wrote 1 file(s)

--- src/components/people/PeopleGridView.tsx
+++ src/components/people/PeopleGridView.tsx
@@ -83,7 +83,7 @@ const PeopleGridView = ({ people, onEditPerson, onDeletePerson, onViewProfile }:
 
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         {currentPeople.map(person => (
           <PersonCard 
             key={person.id} 
