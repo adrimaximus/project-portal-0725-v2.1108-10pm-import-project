f92dfef064279b19a588813a1b79982989f3fb84
adrimaximus <adri@7inked.com>
2025-10-07 09:59:51 +0700

    [dyad] Fixing React Router context error - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from './contexts/ThemeProvider.tsx'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -14,16 +15,18 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <ConfigProvider>
-          <FeaturesProvider>
-            <ThemeProvider defaultTheme="default" defaultMode="system" modeStorageKey="vite-ui-theme-mode" themeStorageKey="vite-ui-theme-palette">
-              <App />
-              <Toaster />
-            </ThemeProvider>
-          </FeaturesProvider>
-        </ConfigProvider>
-      </AuthProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <ConfigProvider>
+            <FeaturesProvider>
+              <ThemeProvider defaultTheme="default" defaultMode="system" modeStorageKey="vite-ui-theme-mode" themeStorageKey="vite-ui-theme-palette">
+                <App />
+                <Toaster />
+              </ThemeProvider>
+            </FeaturesProvider>
+          </ConfigProvider>
+        </AuthProvider>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
