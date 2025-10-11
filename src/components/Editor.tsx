6774ac5a0c4f846e93ffd9008d5a8e5d957c239b
adrimaximus <adri@7inked.com>
2025-10-11 12:42:21 +0700

    Reverted all changes back to version af8d83aa83144e02f530c2b9786b75c5a42f7571

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -12,30 +12,19 @@ export default function Editor({ data, onChange }: EditorProps) {
 
   useEffect(() => {
     async function init() {
+      const { default: Header } = await import("@editorjs/header");
       const { default: List } = await import("@editorjs/list");
       const { default: Warning } = await import("@editorjs/warning");
-      const { default: Header } = await import("editorjs-header-with-alignment");
-      const { default: Paragraph } = await import("editorjs-paragraph-with-alignment");
-      const { default: Alert } = await import("editorjs-alert");
-      const { default: Delimiter } = await import("editorjs-delimiter");
+      const { default: Delimiter } = await import("@editorjs/delimiter");
 
       if (!ref.current) {
         const editor = new EditorJS({
           holder: "editorjs",
-          tools: {
+          tools: { 
             header: {
               class: Header,
               inlineToolbar: true,
-              config: {
-                placeholder: 'Enter a header',
-                levels: [2, 3, 4],
-                defaultAlignment: 'left'
-              }
-            },
-            paragraph: {
-              class: Paragraph,
-              inlineToolbar: true,
-            },
+            }, 
             list: {
               class: List,
               inlineToolbar: true,
@@ -47,15 +36,6 @@ export default function Editor({ data, onChange }: EditorProps) {
               config: {
                 titlePlaceholder: 'Title',
                 messagePlaceholder: 'Message',
-              },
-            },
-            alert: {
-              class: Alert,
-              inlineToolbar: true,
-              shortcut: 'CMD+SHIFT+A',
-              config: {
-                defaultType: 'info',
-                messagePlaceholder: 'Enter something',
               },
             },
             delimiter: Delimiter,
