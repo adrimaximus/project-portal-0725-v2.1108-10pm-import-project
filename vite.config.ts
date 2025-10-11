25ace70f03260af7fbab5bc7997ca5a2d018ecad
adrimaximus <adri@7inked.com>
2025-10-11 13:30:30 +0700

    [dyad] Menerapkan perbaikan untuk galat Lexical dan menyarankan rebuild. - wrote 2 file(s) + extra files edited outside of Dyad

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(() => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: ['react', 'react-dom']
   },
   build: {
     chunkSizeWarningLimit: 1000,
