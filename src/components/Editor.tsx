35b876b8b1c3a2074bf5bf7e2c7f0df4769379de
adrimaximus <adri@7inked.com>
2025-10-11 15:49:51 +0700

    [dyad] Updated editor tools and styles. - wrote 4 file(s), added editorjs-paragraph-with-alignment, editorjs-header-with-alignment package(s)

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { useEffect, useRef } from "react";
 import EditorJS, { OutputData } from "@editorjs/editorjs";
-import Header from "@editorjs/header";
+// @ts-ignore
+import HeaderWithAlignment from "editorjs-header-with-alignment";
+// @ts-ignore
 import List from "@editorjs/list";
-import Paragraph from "@editorjs/paragraph";
-import Quote from "@editorjs/quote";
+// @ts-ignore
+import ParagraphWithAlignment from "editorjs-paragraph-with-alignment";
+// @ts-ignore
 import Delimiter from "@editorjs/delimiter";
 
 interface EditorProps {
@@ -22,7 +25,7 @@ const Editor: React.FC<EditorProps> = ({ data, onChange }) => {
         autofocus: true,
         tools: {
           header: {
-            class: Header,
+            class: HeaderWithAlignment,
             inlineToolbar: true,
             config: {
               levels: [1, 2, 3],
@@ -31,15 +34,11 @@ const Editor: React.FC<EditorProps> = ({ data, onChange }) => {
             },
           },
           paragraph: {
-            class: Paragraph,
+            class: ParagraphWithAlignment,
             inlineToolbar: true,
           },
           list: {
             class: List,
-            inlineToolbar: true,
-          },
-          quote: {
-            class: Quote,
             inlineToolbar: true,
           },
           delimiter: Delimiter,
@@ -67,26 +66,21 @@ const Editor: React.FC<EditorProps> = ({ data, onChange }) => {
   }, []);
 
   return (
-    <div className="w-full border rounded-md p-4 bg-background">
+    <div className="w-full border rounded-md p-4 bg-white">
       <div
         id="editorjs"
         className="
-          leading-snug
-          [&_.ce-block]:my-2
+          text-gray-800 leading-none
+          [&_.ce-block]:my-1
           [&_.ce-header]:font-semibold
           [&_.ce-header[data-level='1']]:text-2xl
           [&_.ce-header[data-level='2']]:text-xl
           [&_.ce-header[data-level='3']]:text-lg
           [&_.ce-paragraph]:text-base
           [&_.ce-paragraph]:leading-tight
-          [&_.ce-quote]:border-l-4
-          [&_.ce-quote]:border-border
-          [&_.ce-quote]:pl-3
-          [&_.ce-quote]:italic
-          [&_.ce-delimiter]:flex
-          [&_.ce-delimiter]:justify-center
-          [&_.ce-delimiter]:text-muted-foreground
-          [&_.ce-delimiter]:before:content-['•••']
+          [&_.ce-delimiter]:h-px
+          [&_.ce-delimiter]:bg-gray-300
+          [&_.ce-delimiter]:my-4
           [&_[data-alignment='left']]:text-left
           [&_[data-alignment='center']]:text-center
           [&_[data-alignment='right']]:text-right
