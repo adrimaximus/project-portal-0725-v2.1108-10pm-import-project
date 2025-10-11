6d7217a06dfa83815e829949e7459618dd502651
adrimaximus <adri@7inked.com>
2025-10-11 18:57:16 +0700

    [dyad] Menampilkan tombol edit tim saat hover - wrote 1 file(s)

--- src/components/project-detail/ProjectTeamCard.tsx
+++ src/components/project-detail/ProjectTeamCard.tsx
@@ -136,11 +136,11 @@ const ProjectTeamCard = ({ project }: ProjectTeamCardProps) => {
 
   return (
     <>
-      <Card>
+      <Card className="group">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Team</CardTitle>
           {!isEditing && (
-            <Button variant="ghost" size="icon" className="h-8 w-8" onClick={() => setIsEditing(true)}>
+            <Button variant="ghost" size="icon" className="h-8 w-8 invisible group-hover:visible" onClick={() => setIsEditing(true)}>
               <Edit className="h-4 w-4" />
             </Button>
           )}
