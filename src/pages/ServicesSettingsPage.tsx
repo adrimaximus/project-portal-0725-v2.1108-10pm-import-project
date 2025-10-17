a24a498c5321003d06142454d3bbaed769d9722e
adrimaximus <adri@7inked.com>
2025-10-17 21:13:44 +0700

    [dyad] Fixing invisible service icons - wrote 1 file(s)

--- src/pages/ServicesSettingsPage.tsx
+++ src/pages/ServicesSettingsPage.tsx
@@ -124,7 +124,7 @@ const ServicesSettingsPage = () => {
                       <TableCell className="font-medium pl-6">
                         <div className="flex items-center gap-3">
                           <div className={cn("h-10 w-10 flex items-center justify-center rounded-lg", service.icon_color)}>
-                            <Icon name={service.icon as any} className="h-5 w-5" />
+                            <Icon name={service.icon as any} className="h-5 w-5 text-white" />
                           </div>
                           {service.title}
                         </div>
