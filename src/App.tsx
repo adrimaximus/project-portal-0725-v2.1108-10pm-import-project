d5d735041fc0b3a760a9897d39cfe71100156ea5
adrimaximus <adri@7inked.com>
2025-10-17 10:16:23 +0700

    Reverted all changes back to version 6be1961e8834a35bc6d074aad201f997ae68ac5f

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Index from './pages/Index';
 import ProjectsPage from './pages/projects/ProjectsPage';
 import { SessionContextProvider } from './contexts/SessionContext';
@@ -6,16 +6,18 @@ import { SessionContextProvider } from './contexts/SessionContext';
 function App() {
   return (
     <SessionContextProvider>
-      <div>
-        <nav className="p-4 bg-gray-100">
-          <Link to="/" className="mr-4">Home</Link>
-          <Link to="/projects">Projects</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-        </Routes>
-      </div>
+      <Router>
+        <div>
+          <nav className="p-4 bg-gray-100">
+            <Link to="/" className="mr-4">Home</Link>
+            <Link to="/projects">Projects</Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+          </Routes>
+        </div>
+      </Router>
     </SessionContextProvider>
   );
 }
