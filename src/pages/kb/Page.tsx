8694cf1ac854085b84d5eaf6771228948860577d
adrimaximus <adri@7inked.com>
2025-10-23 00:02:05 +0700

    [dyad] Applied ChatGPT-style CSS to rich text - wrote 3 file(s)

--- src/pages/kb/Page.tsx
+++ src/pages/kb/Page.tsx
@@ -143,7 +143,7 @@ const Page = () => {
         </div>
 
         <div
-          className="prose dark:prose-invert max-w-none prose-headings:mt-4 prose-headings:mb-1 prose-p:my-1 prose-ul:my-1 prose-li:my-0"
+          className="chatgpt-text"
           dangerouslySetInnerHTML={{ __html: articleContentHtml }}
         />
       </div>
