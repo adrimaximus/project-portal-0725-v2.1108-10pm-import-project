7c4dca7cdace2a4abc868455820808520bf10e76
adrimaximus <adri@7inked.com>
2025-10-27 09:57:02 +0700

    [dyad] Fixing mention module import - wrote 1 file(s)

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -9,9 +9,9 @@ import { Sparkles, RefreshCw } from 'lucide-react';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 // Import and register the mention module
-import Mention from 'quill-mention';
+import * as Mention from 'quill-mention';
 
-Quill.register('modules/mention', Mention);
+Quill.register('modules/mention', Mention as any);
 
 interface MentionableUser {
   id: string;
