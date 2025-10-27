f9dbd646791dbbdd70486ba73fdc927cbe3fa614
adrimaximus <adri@7inked.com>
2025-10-27 09:33:54 +0700

    [dyad] Fixing mobile chat layout overlap - wrote 1 file(s)

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -31,31 +31,31 @@ const ChatPageContent = () => {
     }
   }, [selectedConversation]);
 
-  return (
-    <>
-      {/* Mobile View */}
-      <div className={cn("flex-1 h-full", !isMobile && "hidden")}>
+  if (isMobile) {
+    return (
+      <div className="flex-1 h-full">
         {!selectedConversation ? (
           <ChatList />
         ) : (
           <ChatWindow ref={chatInputRef} onBack={() => selectConversation(null)} />
         )}
       </div>
+    );
+  }
 
-      {/* Desktop View */}
-      <ResizablePanelGroup
-        direction="horizontal"
-        className={cn("flex-1 items-stretch", isMobile ? "hidden" : "flex")}
-      >
-        <ResizablePanel defaultSize={35} minSize={25} maxSize={50} className="min-w-[400px]">
-          <ChatList />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={65}>
-          <ChatWindow ref={chatInputRef} />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </>
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="flex-1 items-stretch"
+    >
+      <ResizablePanel defaultSize={35} minSize={25} maxSize={50} className="min-w-[400px]">
+        <ChatList />
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={65}>
+        <ChatWindow ref={chatInputRef} />
+      </ResizablePanel>
+    </ResizablePanelGroup>
   );
 };
 
