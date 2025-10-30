5ee829767a0fda4d4b280da8047611d0fcab8117
adrimaximus <adri@7inked.com>
2025-10-31 02:59:46 +0700

    [dyad] Fixing cron jobs by removing faulty database functions - wrote 1 file(s)

---
+++ supabase/migrations/0013_remove_cron_trigger_functions.sql
@@ -0,0 +1,3 @@
+DROP FUNCTION IF EXISTS public.trigger_overdue_invoice_reminders();
+DROP FUNCTION IF EXISTS public.trigger_billing_reminders_job();
+DROP FUNCTION IF EXISTS public.process_pending_notifications();
