5959898b5e3c06948634ea5fb72f37dd999e3afd
adrimaximus <adri@7inked.com>
2025-11-03 15:41:49 +0700

    [dyad] Memperbaiki endpoint WBIZTOOL - wrote 1 file(s)

--- supabase/functions/manage-wbiztool-credentials/index.ts
+++ supabase/functions/manage-wbiztool-credentials/index.ts
@@ -9,7 +9,7 @@ const corsHeaders = {
 }
 
 const validateCredentials = async (clientId: string, apiKey: string) => {
-    const wbizResponse = await fetch('https://wbiztool.com/api/v1/get-devices/', {
+    const wbizResponse = await fetch('https://wbiztool.com/api/v1/get-account-info/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
