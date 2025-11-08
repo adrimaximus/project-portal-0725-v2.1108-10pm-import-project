eed6ab142601de54ff831415202460e2987c2d06
adrimaximus <adri@7inked.com>
2025-11-08 17:00:42 +0700

    [dyad] Memperbaiki tautan 'Tasks' di sidebar - wrote 1 file(s)

--- src/components/PortalSidebar.tsx
+++ src/components/PortalSidebar.tsx
@@ -211,7 +211,7 @@ const PortalSidebar = ({ isCollapsed, onToggle }: PortalSidebarProps) => {
             href = '/projects?view=list';
         }
         if (itemNameLower === 'tasks') {
-            href = '/projects?view=tasks-list';
+            href = '/projects?view=tasks';
         }
         if (itemNameLower === 'knowledge base' && href !== '/knowledge-base') {
             href = '/knowledge-base';
