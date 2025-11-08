3d9a88301187c590b306bbe94f79d40a311ae81b
adrimaximus <adri@7inked.com>
2025-11-08 19:14:56 +0700

    [dyad] Making task comment sorting consistent - wrote 1 file(s)

--- src/components/projects/TaskCommentsList.tsx
+++ src/components/projects/TaskCommentsList.tsx
@@ -30,7 +30,7 @@ const TaskCommentsList: React.FC<TaskCommentsListProps> = (props) => {
 
   return (
     <div className="space-y-6">
-      {comments.map((comment) => (
+      {[...comments].reverse().map((comment) => (
         <Comment
           key={comment.id}
           comment={comment}
