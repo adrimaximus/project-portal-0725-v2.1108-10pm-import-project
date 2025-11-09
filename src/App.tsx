11503ada0ede1043d2f873b38ee6bf1aef40c6aa
adrimaximus <adri@7inked.com>
2025-11-09 11:58:49 +0700

    [dyad] Fix AuthProvider import error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
-import AuthProvider from './contexts/AuthContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<DashboardPage />} />
+    </Routes>
   );
 }
 
