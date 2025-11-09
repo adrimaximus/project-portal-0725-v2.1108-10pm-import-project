9a8b312495cf6f51a1e456bedd43abc48446961c
adrimaximus <adri@7inked.com>
2025-11-09 11:59:00 +0700

    Reverted all changes back to version 25c0c1d2757756ee280804608e2d489a6119f818

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
+import AuthProvider from './contexts/AuthContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<DashboardPage />} />
-    </Routes>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
