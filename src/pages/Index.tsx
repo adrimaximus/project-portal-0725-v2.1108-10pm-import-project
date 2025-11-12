a7ef2fc4405f21bfa8b2545b26d9a4e381bda3e8
adrimaximus <adri@7inked.com>
2025-11-12 07:48:58 +0700

    Reverted all changes back to version 4e11fd6d19e92a15a029a480ec9a8c664ab0a471

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,6 @@ import { Skeleton } from "@/components/ui/skeleton";
 import MonthlyProgressChart from "@/components/dashboard/MonthlyProgressChart";
 import UnsplashImage from "@/components/dashboard/UnsplashImage";
 import ActivityHubWidget from "@/components/dashboard/ActivityHubWidget";
-import GoalsSummaryWidget from "@/components/dashboard/GoalsSummaryWidget";
 
 const Index = () => {
   const [date, setDate] = useState<DateRange | undefined>({
@@ -101,10 +100,7 @@ const Index = () => {
               <MonthlyProgressChart projects={filteredProjects} />
               <UnsplashImage />
             </div>
-            <div className="grid gap-6 md:grid-cols-2">
-              <ActivityHubWidget />
-              <GoalsSummaryWidget />
-            </div>
+            <ActivityHubWidget projects={filteredProjects} />
             <DashboardStatsGrid projects={filteredProjects} />
         </div>
       </div>
